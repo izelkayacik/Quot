5264cbf66fb80db0a0dcebb7399e82e48b0e42fc
izelkayacik <izelkayacik@hotmail.com>
2022-07-10 21:51:11 +0300

    Issue2
    Handle share intent in the activity

--- Quot/app/src/main/java/com/izelkayacik/quot/MainActivity.kt
+++ Quot/app/src/main/java/com/izelkayacik/quot/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.izelkayacik.quot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handleIntent(intent)
+
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        handleIntent(intent)
+    }
+
+    fun handleIntent(intent: Intent){
+        println(intent?.action)
+        println(intent?.type)
+        println(intent?.getStringExtra(Intent.EXTRA_TEXT))
+
+        if (intent?.action == Intent.ACTION_SEND && intent?.type == "text/plain"){
+            intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+                findViewById<TextView>(R.id.textView).text = it.split("\\n[\\r]?".toRegex())[0]
+            }
+        }
     }
 }
