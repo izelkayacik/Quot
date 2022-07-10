2f4859255538b700bf6379e42b2f2ce5d9d40905
izelkayacik <izelkayacik@hotmail.com>
2022-07-10 19:48:21 +0300

    first commit

---
+++ Quot/app/src/main/java/com/izelkayacik/quot/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.izelkayacik.quot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
